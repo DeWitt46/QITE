429673770fa80892c66f6fb604324b645a90c37b
Carlo <97054669+DeWitt46@users.noreply.github.com>
2024-04-16 18:22:43 +0200

    Add files via upload

--- code/MHETS_script.py
+++ code/MHETS_script.py
@@ -19,36 +19,34 @@ from qiskit.providers.fake_provider import FakeManila
 from library import setup
 from library.MHETS import MHETS_instance
 from library.operator_creation import LMG_hamiltonian
-from library.ansatz_creation import two_local
+from library.ansatz_creation import two_local, pma
 
 
-for N in [4]:
-    for gy in [0.0]:
+flag = "noise"
+for N in [3]:
+    for gy in [0.49]:
         for B in [0.2]:
-            for shots in [100]:
+            for shots in [100]:  # Ignore If flag == statevector
+                print("----------------------------------------")
                 print("Simulation for N = {}, gy = {}, B = {}".format(N, gy, B))
-                # N = 3
-                # gy = 0.5
-                # B = 0.1
+                print("----------------------------------------")
                 first_beta = 0.2
                 last_beta = 5.0
-                num_beta_points = 1
+                num_beta_points = 3
                 betas = np.linspace(first_beta, last_beta, num_beta_points)
-                maxiter = 30
-                optimizer = "COBYLA"
-                tol = 1.5e-1
-                # shots = 1000  # Ignore If flag == statevector
+                maxiter = 25
+                optimizer = "spsa"
+                tol = 1e-4
                 H = LMG_hamiltonian(N, gy, B)
-                ancilla_ansatz = two_local(num_qubits=N, num_reps=1, entanglement="linear")
-                system_ansatz = two_local(
-                    num_qubits=N, num_reps=2, entanglement="linear", par_name="y"
+                ancilla_ansatz = two_local(
+                    num_qubits=N, num_reps=1, entanglement="linear"
+                )
+                system_ansatz = pma(
+                    num_qubits=N, num_reps=0, architecture="linear", par_name="y"
                 )
 
-                flag = "qasm"
-                run_flag = (
-                    False  # True if you want some randomized starting point for minimization
-                )
-                n_starting_point = 10  # Ignore if run_flag is False
+                run_flag = True  # True if you want some randomized starting point for minimization
+                n_starting_point = 3  # Ignore if run_flag is False
                 model_tag = None
 
                 if flag == "noise":
@@ -57,30 +55,29 @@ for N in [4]:
                     model_tag = "ibm_osaka"
                 backend = setup.setup_backend(flag=flag, model_tag=model_tag)
                 path = "./MHETS_data/"
-                file_name = setup.setup_file_name(H=H, flag=flag, shots=shots)
                 initial_parameter_list = setup.setup_initial_parameter_list(
                     H=H, flag=flag, num_beta_points=len(betas), path=path
                 )
-
-                optimization_options = {
-                    "ancilla_ansatz": "two_local",
-                    "ancilla_num_reps": ancilla_ansatz.get_num_reps(),
-                    "ancilla_ansatz_entanglement": ancilla_ansatz.get_entanglement(),
-                    "ancilla_ansatz_rotation_blocks": ancilla_ansatz.get_rotation_blocks(),
-                    "ancilla_ansatz_entanglement_blocks": ancilla_ansatz.get_entanglement_blocks(),
-                    "system_ansatz": "two_local",
-                    "system_num_reps": system_ansatz.get_num_reps(),
-                    "system_ansatz_entanglement": system_ansatz.get_entanglement(),
-                    "system_ansatz_entanglement_blocks": system_ansatz.get_entanglement_blocks(),
-                    "system_ansatz_rotation_blocks": system_ansatz.get_rotation_blocks(),
-                    "maxiter": maxiter,
-                    "optimizer": optimizer,
-                    "initial_parameter_list": initial_parameter_list,
-                    "flag": flag,
-                    "tol": tol,
-                    "shots": shots,
-                }
-
+                optimization_options = setup.setup_optimization_options(
+                    ancilla_ansatz=ancilla_ansatz,
+                    system_ansatz=system_ansatz,
+                    maxiter=maxiter,
+                    optimizer=optimizer,
+                    initial_parameter_list=initial_parameter_list,
+                    flag=flag,
+                    tol=tol,
+                    shots=shots,
+                )
+                pma_flag = False
+                if optimization_options["system_ansatz"] == "pma":
+                    pma_flag = True
+                file_name = setup.setup_file_name(
+                    H=H, flag=flag, shots=shots, pma_flag=pma_flag
+                )
+                if flag == "noise":
+                    file_name = file_name.replace(
+                        ".pickle", "_{}.pickle".format(optimizer)
+                    )
                 mhets = MHETS_instance(
                     H=H,
                     ancilla_ansatz=ancilla_ansatz,
@@ -91,7 +88,10 @@ for N in [4]:
                 )
 
                 if run_flag is True:
-                    multi_beta_result, multi_beta_runs = mhets.multi_beta_optimization_run(
+                    (
+                        multi_beta_result,
+                        multi_beta_runs,
+                    ) = mhets.multi_beta_optimization_run(
                         betas=betas, n_starting_point=n_starting_point
                     )
                 else:
@@ -100,10 +100,14 @@ for N in [4]:
                             old_data = pickle.load(f)
                     except FileNotFoundError:
                         print("No file found. Optimize from scratch")
-                        multi_beta_result = mhets.multi_beta_optimization_from_scratch(betas)
+                        multi_beta_result = mhets.multi_beta_optimization_from_scratch(
+                            betas
+                        )
                     else:
                         print("File found. Append results")
-                        new_betas = setup.setup_betas(old_betas=old_data["betas"], betas=betas)
+                        new_betas = setup.setup_betas(
+                            old_betas=old_data["betas"], betas=betas
+                        )
                         print("old_betas", old_data["betas"])
                         print("betas_inserted", betas)
                         print("final_beta_list", new_betas)
@@ -112,7 +116,8 @@ for N in [4]:
                         )
                 # WRITING DATA TO FILE
                 path = "./MHETS_data/"
-                file_name = setup.setup_file_name(H, flag, shots=shots)
+                # IF YOU WANT TO CHANGE SOMETHING IN THE FILE NAME
+                # file_name = setup.setup_file_name(H, flag, shots=shots, pma_flag=pma_flag)
                 with open(path + file_name, "wb") as f:
                     # Pickle the preparation_result dictionary using the highest protocol available.
                     pickle.dump(multi_beta_result, f, pickle.HIGHEST_PROTOCOL)
@@ -124,6 +129,10 @@ for N in [4]:
                                 H.N, H.gy, H.B, run_index
                             ).replace(".", "")
                         ) + ".pickle"
+                        if pma_flag == True:
+                            file_name.replace(".pickle", "_pma.pickle")
                         with open(path + file_name, "wb") as f:
                             # Pickle the preparation_result dictionary using the highest protocol available.
-                            pickle.dump(multi_beta_runs[run_index], f, pickle.HIGHEST_PROTOCOL)
+                            pickle.dump(
+                                multi_beta_runs[run_index], f, pickle.HIGHEST_PROTOCOL
+                            )
--- code/off_line_analysis.py
+++ code/off_line_analysis.py
@@ -16,16 +16,18 @@ from library import plotting, setup
 from library.operator_creation import LMG_hamiltonian
 from library.MHETS import MHETS_instance
 
-flag = "statevector"
-N = 4
-gy = 0.0
+flag = "noise"
+backend = None  # Only for graphic purpose
+N = 3
+gy = 0.49
 B = 0.2
 H = LMG_hamiltonian(N, gy, B)
 
 
 path = "./MHETS_data/"
-file_name = setup.setup_file_name(H, flag, shots=10)
-# IF run_flag WAS TRUE
+file_name = setup.setup_file_name(H, flag, shots=100, pma_flag=True)
+file_name = file_name.replace(".pickle", "_cobyla.pickle")
+# IF run_flag WAS TRUE AND YOU WANT TO LOOK AT A SINGLE RUN
 # run_index = 3
 # file_name = (
 #     "MHETS_{}at_gy{}_B{}_run{}".format(H.N, H.gy, H.B, run_index).replace(".", "")
@@ -35,7 +37,6 @@ file_name = setup.setup_file_name(H, flag, shots=10)
 # IF PLOTTING SINGLE MULTI_BETA_RESULT
 with open(path + file_name, "rb") as f:
     multi_beta_result = pickle.load(f)
-
 optimization_options = multi_beta_result["optimization_options"]
 try:
     a = optimization_options["shots"]
@@ -53,18 +54,20 @@ for index in range(len(beta_list)):
     mhets.update_parameters(multi_beta_result["optimized_parameter_list"][index])
     rho = mhets.QST(mhets.build_total_circuit(), shots=optimization_options["shots"])
     rho_s_list.append(partial_trace(rho, range(ancilla_ansatz.get_num_qubits())))
-# backend variable is just for the name that has to be in the plots, no calculations
 plotting.plot_MHETS_thermal_average(
-    beta_list, rho_s_list, H, backend="FakeManila", beta_final=10
+    beta_list, rho_s_list, H, backend=backend, beta_final=10
 )
-plotting.plot_fidelity(beta_list, rho_s_list, H, backend="FakeManila")
+plotting.plot_fidelity(beta_list, rho_s_list, H, backend=backend)
+plotting.plot_rel_entropy(beta_list, rho_s_list, H, backend=backend)
+plotting.plot_parity(beta_list, rho_s_list, H, backend=backend)
 
-for index in range(len(beta_list)):
-    print("beta*Helmoltz energy expected vs simulated")
-    print(H.cost_function(beta_list[index]), multi_beta_result["Helmoltz energy"][index])
-    print("")
 # print(multi_beta_result.keys())
-print(multi_beta_result["optimized_parameter_list"])
+# print(multi_beta_result["callback_data"][0][0].analysis_results("state_fidelity"))
+print(multi_beta_result["n_eval"])
+print(multi_beta_result["Time optimization"])
+print(multi_beta_result["optimization_options"])
+
+
 # FOR NOISE SIMULATIONS
 # for data in multi_beta_result["callback_data"][0]:
 # data = multi_beta_result["callback_data"][0][0]
@@ -82,6 +85,6 @@ print(multi_beta_result["optimized_parameter_list"])
 #             for shots in [10, 100, 1000, 10000]:
 #                 file_names.append(setup.setup_file_name(H, flag, shots))
 
-# multi_data = setup.setup_offline_multi_data(path, file_names)
+# multi_data = setup.loading_offline_multi_data(path, file_names)
 # plotting.plot_MHETS_shots(multi_data=multi_data, betas=np.linspace(0.2, 5.0, 3), H=H)
 # plotting.plot_MHETS_shots_fidelity(multi_data=multi_data, betas=np.linspace(0.2, 5.0, 3), H=H)
